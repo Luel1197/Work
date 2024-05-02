1ca5c8e0f8deca88f272209db3e151b872fe93d3
Luel1197 <sonael121@naver.com>
2024-05-02 19:31:42 +0900

    do while good

--- chap1/src/App.java
+++ chap1/src/App.java
@@ -3,23 +3,21 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        System.out.println("첫 숫자 입력");
-        int num1 = sc.nextInt();
-
-        System.out.println("두번째 입력");
-        int num2 = sc.nextInt();
-
-        System.out.println("사칙연산 기호 입력");
-        char op = sc.next().charAt(0);
-
-        int result = 0;
-
-
-
-
-
         do {
+            System.out.println("첫 숫자 입력");
+            int num1 = sc.nextInt();
+
+            System.out.println("두번째 입력");
+            int num2 = sc.nextInt();
+
+            System.out.println("사칙연산 기호 입력");
+            char op = sc.next().charAt(0);
+
+            int result = 0;
+
+            String exit = "exit";
+
+
             switch (op) {
                 case '+':
                     result = num1 + num2;
@@ -43,10 +41,11 @@ public class App {
                 default:
                     System.out.println("올바른 사칙연산 기호가 아닙니다.");
             }
-            System.out.println("답은 : "+ result);
+            System.out.println("답은 : " + result);
 
             System.out.println("종료하시겠습니까? (exit 입력시 종료) ");
-        } while ();
+
+        } while (!sc.next().equals("exit"));
 
     }
 }
