ea3dd06a8cb5cf8ac5f8e512a40e3761c366e808
Luel1197 <sonael121@naver.com>
2024-05-02 20:38:55 +0900

    lv2 1 good

--- chap1/src/App.java
+++ chap1/src/App.java
@@ -7,7 +7,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        List<Integer> results = new ArrayList<>();
+
 
 
 
---
+++ chap1/src/Calculator.java
@@ -0,0 +1,36 @@
+import javax.naming.OperationNotSupportedException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+    List<Integer> results = new ArrayList<>();
+
+    public int calculate(int num1, int num2, char op) {
+        int result = 0;
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+
+            case '-':
+                result = num1 - num2;
+                break;
+
+            case '*':
+                result = num1 * num2;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    throw new ArithmeticException("나눗셈에서 분모가 0인 오류");
+                }
+                result = num1 / num2;
+                break;
+
+            default:
+
+                throw new UnsupportedOperationException("올바른 사칙연산 기호가 아닙니다.");
+        }
+    }
+}
