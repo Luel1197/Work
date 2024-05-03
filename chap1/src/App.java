02b5fe2f1173a3dc1942ab5611656f9c17f3494c
Luel1197 <sonael121@naver.com>
2024-05-03 12:56:43 +0900

    lv2 -7

--- chap1/src/App.java
+++ chap1/src/App.java
@@ -9,7 +9,7 @@ public class App {
 
 
 
-        Calculator calc = new Calculator(new ArrayList<>());
+        Calculator calc = new Calculator(new ArrayList<>(), new ArrayList<>());
 
 //        //매개 변수가 1개인것을 예측했는데 아무것도 없다 기본 생성자가 없다 컴파일러가 만들어주지 않는다.
 //        Calculator calc2 = new Calculator();
@@ -19,35 +19,57 @@ public class App {
 
 
         do {
-            System.out.println("첫 숫자 입력");
-            int num1 = sc.nextInt();
 
-            System.out.println("두번째 입력");
-            int num2 = sc.nextInt();
+            System.out.println("1: 원의 넓이 구하기, 2: 사칙연산");
+            int choice = sc.nextInt();
+            if (choice == 1) {
+                System.out.println("원의 반지름을 입력하세욧");
+                int radius = sc.nextInt();
 
-            System.out.println("사칙연산 기호 입력");
-            char op = sc.next().charAt(0);
+                double area = calc.calculateArea(radius);
+                calc.getAreaResult().add(area);
+
+                System.out.println("원의 넓이는" + area);
+
+                System.out.println("저장된 원의 넓이 들을 전부 보시겠습니까? (inquiry) " );
+                if (sc.next().equals("inquiry")) {
+                    calc.inquiryArea();
+                }
+
+            } else if (choice == 2) {
+                System.out.println("첫 숫자 입력");
+                int num1 = sc.nextInt();
+
+                System.out.println("두번째 입력");
+                int num2 = sc.nextInt();
+
+                System.out.println("사칙연산 기호 입력");
+                char op = sc.next().charAt(0);
 
 
-            int result = calc.calculate(num1, num2, op);
+                int result = calc.calculate(num1, num2, op);
 
-            calc.getResults().add(result);
+                calc.getResults().add(result);
 
-            System.out.println("답은 : " + result);
+                System.out.println("답은 : " + result);
 
 
-            //요청 0번 칸 리무브 리절트 명령어 실행 요청
-            System.out.println("가장 첫번째 결과 삭제하나요? (remove)");
-            if (sc.next().equals("remove")) {
-                calc.removeResult(0);
+                //요청 0번 칸 리무브 리절트 명령어 실행 요청
+                System.out.println("가장 첫번째 결과 삭제하나요? (remove)");
+                if (sc.next().equals("remove")) {
+                    calc.removeResult(0);
+                }
+
+
+
+                System.out.println("저장된 연산결과 조회? (inquiry) " );
+                if (sc.next().equals("inquiry")) {
+                    calc.inquiryResults();
+                }
+            } else {
+                throw new UnsupportedOperationException("오류");
             }
 
-
-
-            System.out.println("저장된 연산결과 조회? (inquiry) " );
-            if (sc.next().equals("inquiry")) {
-                calc.inquiryResults();
-            }
 
             System.out.println();
             System.out.println("종료하시겠습니까? (exit 입력시 종료) ");
--- chap1/src/Calculator.java
+++ chap1/src/Calculator.java
@@ -1,16 +1,29 @@
 import javax.naming.OperationNotSupportedException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Calculator {
-    private List<Integer> results ;
 
+    private static final double PI = 3.14;
+    private List<Integer> results ;
+    private List<Double> areaResult ;
 
     //오버로딩 생성자
     //생성자를 통해서도 필드값을 초기화 가능
     //오버로딩 된 생성자가 생기면 기본 생성자가 생기지 않음
-    public Calculator(List<Integer> results) {
-       this.results = results
+    public Calculator(List<Integer> results, List<Double> areaResult) {
+       this.results = results;
+       this.areaResult = areaResult;
+    }
+
+
+    public List<Double> getAreaResult() {
+        return areaResult;
+    }
+
+    public void setAreaResult(List<Double> areaResult) {
+        this.areaResult = areaResult;
     }
 
 
@@ -52,9 +65,23 @@ public class Calculator {
 
 
 
-
         return result;
     }
+
+    /**
+     *
+     * @param radius : 원의 반지름
+     * @return : 원의 넓이 (3.14 * 반지름 * 반지름)
+     */
+    public double calculateArea(int radius) {
+        return PI * radius * radius;
+    }
+
+
+
+
+
+
 
     public void removeResult(int index) {
         this.results.remove(index);
@@ -64,6 +91,12 @@ public class Calculator {
     public void inquiryResults() {
         for (Integer res : results) {
             System.out.print(res + ", ");
+        }
+    }
+
+    public void inquiryArea(){
+        for (Double areaResult : areaResult) {
+            System.out.println("원의넓이" + areaResult);
         }
     }
 }
